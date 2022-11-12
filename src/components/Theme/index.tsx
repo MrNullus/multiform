0637e1f5b8451fae8859599254c94946865f1206
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-11-12 18:29:09 -0300

    changes

--- src/components/Theme/index.tsx
+++ src/components/Theme/index.tsx
@@ -1,14 +1,21 @@
 import { ReactNode } from 'react';
 
+import { useForm } from '../../contexts/FormContext';
+
 import * as C from './styles.ts';
 
+import { SidebarItem } from '../SidebarItem';
 import { Header } from '../Header/index.tsx';
 
 type Props = { 
 	children: ReactNode 
 };
 
+
 export const Theme = ({ children }: Props) => {
+
+	const [ state ] = useForm();
+
 	return (	
 		<C.Container>
 
@@ -18,7 +25,29 @@ export const Theme = ({ children }: Props) => {
 				<C.Steps>
 					
 					<C.Sidebar>
-						
+						<C.SidebarItem  
+							title="Pessoal"
+							description="Se identifique"
+							icon="profile"
+							path="/"
+							active={state.currentStep === 1}
+						/>						
+
+						<C.SidebarItem  
+							title="Profissional"
+							description="Seu nível"
+							icon="book"
+							path="/step2"
+							active={state.currentStep === 2}
+						/>
+
+						<C.SidebarItem  
+							title="Contatos"
+							description="Como te achar"
+							icon="mail"
+							path="/step3"
+							active={state.currentStep === 3}
+						/>
 					</C.Sidebar>
 
 					<C.Page>
